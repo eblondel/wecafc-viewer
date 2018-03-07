ae6ac5a6e1dd95f78f5bc23654a25355a9ba610b
eblondel <emmanuel.blondel1@gmail.com>
2018-03-08 00:17:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# wecafc-viewer
+
+**WECAFC-FIRMS prototype atlas**
+
+The WECAFC-FIRMS atlas prototype is a web data and map viewer allowing to query Caribbean regional fisheries datasets (timeseries), visualize and share data.
+
+##Fundings
+
+_This work has received funding from the European Union's Horizon 2020 research and innovation programme under the BlueBRIDGE project (Grant agreement No 675680)._
+
+
---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+/**
+ * WECAFC Fisheries data viewer 
+ * Application development funded by BlueBridge EC project
+ * 
+ * @author Emmanuel Blondel GIS & Marine web-information systems Expert
+ *		   Contact: https://eblondel.github.io / emmanuel.blondel1@gmail.com 
+ */
+
+var app = app || {};
+var app = "1.0-beta"
+ 
+$(document).ready(function(){
+	app = new OpenFisViewer({
+		OGC_CSW_BASEURL: "http://wecafc-firms.d4science.org/geonetwork/srv/eng/csw",
+		OGC_CSW_SCHEMA : "http://www.isotc211.org/2005/gmd",
+		OGC_WMS_LAYERS : [
+			{
+				group: 0, id: "eez", title: "EEZ boundaries",
+				wmsUrl: "http://wecafc-firms.d4science.org/geoserver/wecafc/wms", layer: "wecafc:eez_overlap_strict",
+				visible: true, showLegend: false, opacity: 0.6, tiled: true, cql_filter: undefined
+			},	
+			{
+				group: 0, id: "fsa", title: "FAO major areas & breakdown",
+				wmsUrl: "http://www.fao.org/figis/geoserver/area/wms", layer: "area:FAO_AREAS",
+				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: undefined
+			},
+			{
+				group: 0, id: "marineareas", title: "Marine areas",
+				wmsUrl: "http://www.fao.org/figis/geoserver/fifao/wms", layer: "fifao:MarineAreas",
+				visible: true, showLegend: true, opacity: 0.9, tiled: true, cql_filter: undefined
+			}
+		]
+	},{
+		catalog : {
+			maxitems: 25,
+			filters: [
+				{name: 'dc:subject', value: '%Fisheries%'}
+			]
+		},
+		map : {
+			extent: [-140.37817293406107, -18.569372534752684, -5.378172934061055, 45.94234621524731],
+			zoom: 4,
+			layergroups : [{name: "Base overlays"},{name: "Fisheries maps"}]
+		},
+		ui 	: { time: 'slider'}
+	});
+	app.init();
+});
