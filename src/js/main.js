2f25d41a1b0c95ba372475b1b083d8045341bc62
eblondel <emmanuel.blondel1@gmail.com>
2018-05-21 21:56:40 +0200

    change layer config

--- src/js/main.js
+++ src/js/main.js
@@ -15,19 +15,42 @@ $(document).ready(function(){
 		OGC_CSW_SCHEMA : "http://www.isotc211.org/2005/gmd",
 		OGC_WMS_LAYERS : [
 			{
+				group: 0, id: "shelf", title: "Continental Shelf",
+				wmsUrl: "http://www.fao.org/figis/geoserver/wms", layer: "fifao:SHELF_ERASE",
+				visible: true, showLegend: true, opacity: 0.8, tiled: true, cql_filter: undefined
+			},
+
+			{
 				group: 0, id: "eez", title: "EEZ boundaries",
 				wmsUrl: "https://wecafc-firms.d4science.org/geoserver/wecafc/wms", layer: "wecafc:eez_overlap_strict",
-				visible: true, showLegend: true, opacity: 0.6, tiled: true, cql_filter: undefined
+				visible: false, showLegend: true, opacity: 0.6, tiled: true, cql_filter: undefined
 			},
 			{
-				group: 0, id: "fsa2", title: "FAO Subarea 41.1 and breakdown",
+				group: 0, id: "intersect_fsa_eez", title: "FAO Areas* - EEZ Intersects <br><small style='margin-left: 22px'>(*including breakdown in draft stage, not endorsed by CWP)</small>",
+				wmsUrl: "http://www.fao.org/figis/geoserver/fifao/wms", layer: "fifao:FAO_AREAS_x_EEZ_HIGHSEAS",
+				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: "F_AREA = 31"
+			},
+			{
+				group: 0, id: "fsa_41", title: "FAO Subarea 41.1 and breakdown",
 				wmsUrl: "http://www.fao.org/figis/geoserver/area/wms", layer: "area:FAO_AREAS",
 				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: "F_SUBAREA = '41.1'"
 			},
 			{
-				group: 0, id: "fsa1", title: "FAO Major Area 31 and breakdown* <br><small style='margin-left: 22px'>(*draft stage, not endorsed by CWP)</small>",
+				group: 0, id: "fsa_31_division", title: "FAO Major Area 31 - Divisions* <br><small style='margin-left: 22px'>(*draft stage, not endorsed by CWP)</small>",
 				wmsUrl: "http://www.fao.org/figis/geoserver/fifao/wms", layer: "fifao:FAO_AREAS",
-				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: "F_AREA = 31"
+				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: "F_AREA = 31 AND F_LEVEL = 'DIVISION'",
+				style: 'all_fao_areas_orange'
+			},
+			{
+				group: 0, id: "fsa_31_subarea", title: "FAO Major Area 31 - Subareas* <br><small style='margin-left: 22px'>(*draft stage, not endorsed by CWP)</small>",
+				wmsUrl: "http://www.fao.org/figis/geoserver/fifao/wms", layer: "fifao:FAO_AREAS",
+				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: "F_AREA = 31 AND F_LEVEL = 'SUBAREA'",
+				style: 'all_fao_areas_blue'
+			},
+			{
+				group: 0, id: "fsa_31_major", title: "FAO Major Area 31",
+				wmsUrl: "http://www.fao.org/figis/geoserver/fifao/wms", layer: "fifao:FAO_AREAS",
+				visible: true, showLegend: true, opacity: 0.9, tiled: true, cql_filter: "F_AREA = 31 AND F_LEVEL = 'MAJOR'"
 			},	
 			{
 				group: 0, id: "wecafc", title: "WECAFC Competence area",
@@ -42,12 +65,12 @@ $(document).ready(function(){
 			{
 				group: 2, id: "firms-resources", title: "Marine resources",
 				wmsUrl: "http://www.fao.org/figis/geoserver/firms/wms", layer: "firms:resource_all_points",
-				visible: true, showLegend: true, opacity: 0.9, tiled: false, cql_filter: "AGENCY = 'WECAFC'",
+				visible: false, showLegend: true, opacity: 0.9, tiled: false, cql_filter: "AGENCY = 'WECAFC'",
 				style: 'point_resource_cluster'
 			},{
 				group: 2, id: "firms-fisheries", title: "Fisheries",
 				wmsUrl: "http://www.fao.org/figis/geoserver/firms/wms", layer: "firms:fishery_all_points",
-				visible: true, showLegend: true, opacity: 0.9, tiled: false, cql_filter: "AGENCY = 'WECAFC'",
+				visible: false, showLegend: true, opacity: 0.9, tiled: false, cql_filter: "AGENCY = 'WECAFC'",
 				style: 'point_fishery_cluster'
 			}
 		]
